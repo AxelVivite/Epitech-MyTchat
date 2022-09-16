abeb0ff0d380a453a197f9d91d2eaa2f38e376cc
Axel Virot <axel.virot@epitech.eu>
2022-09-16 15:39:13 +0200

    [MERGE]

--- frontend/src/components/layouts/pageLayout/drawer/Drawer.tsx
+++ frontend/src/components/layouts/pageLayout/drawer/Drawer.tsx
@@ -24,7 +24,7 @@ const Drawer = (props: DrawerProps): JSX.Element => {
                 height: "64px"
             }}>
                 <Typography>Conversations</Typography>
-                
+
                 <IconButton onClick={props.handleClickDrawerButton}>
                     <AddIcon />
                 </IconButton>
@@ -36,33 +36,29 @@ const Drawer = (props: DrawerProps): JSX.Element => {
 
     return (
         <>
-        {
-                <>
-                    <MuiDrawer id="drawer"
-                        variant="temporary"
-                        open={props.isDrawerOpen}
-                        onClose={props.handleClickDrawerButton}
-                        ModalProps={{
-                            keepMounted: true,
-                        }}
-                        sx={{
-                            display: { xs: "block", md: "none" },
-                            "& .MuiDrawer-paper": { width: "280px" },
-                        }}
-                    >
-                        {drawer}
-                    </MuiDrawer>
-                    <MuiDrawer id="drawer"
-                        variant="permanent"
-                        sx={{
-                            display: { xs: "none", md: "block" },
-                            "& .MuiDrawer-paper": { width: "280px" },
-                        }}
-                    >
-                        {drawer}
-                    </MuiDrawer>
-                </>
-        }
+            <MuiDrawer id="drawer"
+                variant="temporary"
+                open={props.isDrawerOpen}
+                onClose={props.handleClickDrawerButton}
+                ModalProps={{
+                    keepMounted: true,
+                }}
+                sx={{
+                    display: { xs: "block", md: "none" },
+                    "& .MuiDrawer-paper": { width: "280px" },
+                }}
+            >
+                {drawer}
+            </MuiDrawer>
+            <MuiDrawer id="drawer"
+                variant="permanent"
+                sx={{
+                    display: { xs: "none", md: "block" },
+                    "& .MuiDrawer-paper": { width: "280px" },
+                }}
+            >
+                {drawer}
+            </MuiDrawer>
         </>
     );
 };
---
+++ frontend/src/services/translation/i18l.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import transFr from "../../constants/languages/fr.json";
+import transEn from "../../constants/languages/en.json";
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+        resources: {
+            en: transEn,
+            fr: transFr
+        }
+    });
+
+export default i18n;
