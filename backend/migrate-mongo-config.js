627ea9f4ffd2cc3f2e026efb37a60ad191fd6663
Alexis Auriac <alexis.auriac@epitech.eu>
2022-09-28 11:34:13 +0200

    Fixed lint errors

--- backend/migrate-mongo-config.js
+++ backend/migrate-mongo-config.js
@@ -1,15 +1,15 @@
 export default {
   mongodb: {
     // todo: change db url
-    url: "mongodb://localhost:27017",
+    url: 'mongodb://localhost:27017',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }
+    },
   },
-  migrationsDir: "migrations",
-  changelogCollectionName: "changelog",
-  migrationFileExtension: ".js",
+  migrationsDir: 'migrations',
+  changelogCollectionName: 'changelog',
+  migrationFileExtension: '.js',
   useFileHash: false,
   moduleSystem: 'commonjs',
 };
