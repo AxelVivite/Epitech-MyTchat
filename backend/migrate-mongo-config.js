91af142cd031045d5bcb88648ff0db7878d191fe
Alexis Auriac <alexis.auriac@epitech.eu>
2022-09-28 11:25:34 +0200

    Made migration system

---
+++ backend/migrate-mongo-config.js
@@ -0,0 +1,15 @@
+export default {
+  mongodb: {
+    // todo: change db url
+    url: "mongodb://localhost:27017",
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  },
+  migrationsDir: "migrations",
+  changelogCollectionName: "changelog",
+  migrationFileExtension: ".js",
+  useFileHash: false,
+  moduleSystem: 'commonjs',
+};
