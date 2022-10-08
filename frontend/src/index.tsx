836bc1e38e6e719c5425c9553b8318cadce10f59
David TEDESCO <davidtedesco@MacBook-Pro-de-David.local>
2022-10-08 13:13:15 +0200

    fix: remove redux from the app.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Global State Manager
-import { Provider } from 'react-redux';
 import "./services/translation/i18l";
 
 const root = ReactDOM.createRoot(
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
