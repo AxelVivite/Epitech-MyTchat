02e3c513bef79b182baef1e16d6dc915c2889960
David TEDESCO <davidtedesco@MacBook-Pro-de-David.local>
2022-10-08 13:11:27 +0200

    fix: remove old import use for redux on index.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Global State Manager
 import { Provider } from 'react-redux';
-import store from './store';
 import "./services/translation/i18l";
 
 const root = ReactDOM.createRoot(
