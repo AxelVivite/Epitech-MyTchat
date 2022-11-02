dbb0b632b5097ea70526ef9cfc71eac492819374
David TEDESCO <davidtedesco@macbook-pro-de-david.home>
2022-11-02 22:07:09 +0100

    merge: last conflict with the gsm init branch here

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Global State Manager
 import "./services/translation/i18l";
 import { GlobalStateProvider } from './utils/globalStateManager/globalStateInit';
-=======
-
-import App from 'App';
-import reportWebVitals from 'reportWebVitals';
-
-import 'index.css';
-import 'services/translation/i18n';
->>>>>>> ec5059aaf3971f9a44a4563bae2eaa4e9d85bc14
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -22,13 +13,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <GlobalStateProvider>
       <App />
     </GlobalStateProvider>
-=======
-    <App />
->>>>>>> ec5059aaf3971f9a44a4563bae2eaa4e9d85bc14
   </React.StrictMode>
 );
 
