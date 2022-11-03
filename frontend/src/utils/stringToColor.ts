110b0fa2d417cf7f445bde4dc1726688ef34a387
Axel Virot <Vivite@vivitour.home>
2022-11-03 15:41:40 +0100

    Added bitwise function for rbnb

--- frontend/src/utils/stringToColor.ts
+++ frontend/src/utils/stringToColor.ts
@@ -1,13 +1,19 @@
+import {
+  zeroFillLeftShift,
+  andBitOperator,
+  zeroFillRightShift,
+} from './bitwise';
+
 const stringToColor = (str: string): string => {
   let hash = 0;
   let color = '#';
   let i;
 
   for (i = 0; i < str.length; i += 1) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    hash = str.charCodeAt(i) + ((zeroFillLeftShift(hash, 5)) - hash);
   }
   for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
+    const value = andBitOperator(zeroFillRightShift(hash, (i * 8)), 0xff);
     color += `00${value.toString(16)}`.slice(-2);
   }
   return color;
