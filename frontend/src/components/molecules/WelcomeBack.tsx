d0900268a6ce83b4f72d552b38c36228106a7643
Axel Virot <Vivite@vivitour.home>
2022-11-06 05:22:41 +0100

    Added translation for welcome back

--- frontend/src/components/molecules/WelcomeBack.tsx
+++ frontend/src/components/molecules/WelcomeBack.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import {
   Box,
@@ -9,6 +10,7 @@ import { useGlobalState } from '../../utils/globalStateManager/globalStateInit';
 
 const WelcomeBack = function WelcomeBack(): React.ReactElement<unknown, string> | null {
   const { state } = useGlobalState();
+  const { t } = useTranslation();
 
   return (
     <Box
@@ -18,7 +20,7 @@ const WelcomeBack = function WelcomeBack(): React.ReactElement<unknown, string> 
       <Box style={{ marginBottom: '20vh' }}>
         <Card className="col p--16 width--280 flex--center-align m--auto">
           <p style={{ fontSize: '24px' }} className="mb--16 mt--8">
-            &#11088;  Welcome back  &#11088;
+            &#11088;  {t('welcome_back')}  &#11088;
           </p>
           <p style={{ fontSize: '32px' }} className="mb--24">
             {state.user?.username}
