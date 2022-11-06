0112fedbe207dba2d891c6770cc7a963a753ca15
Axel Virot <Vivite@vivitour.home>
2022-11-06 04:15:21 +0100

    Added 404 styling

--- frontend/src/components/views/PageNotFound.tsx
+++ frontend/src/components/views/PageNotFound.tsx
@@ -1,10 +1,41 @@
 import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  Box,
+  Button,
+  Card,
+} from '@mui/material';
 
 const PageNotFound = function PageNotFound(): ReactElement<unknown, string> | null {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (localStorage.getItem('darkTheme') === 'true') {
+      document.body.classList.add('dark-theme');
+    }
+  }, []);
+
   return (
-    <div className="Test">
-      <text>404 Not found</text>
-    </div>
+    <Box>
+      <Card className="col flex--center-align div--centered p--24">
+        <p style={{ fontSize: '64px' }} className="mt--8 mb--16">404</p>
+        <p style={{ fontSize: '30px' }} className="mb--24">
+          {t('page_not_found')}
+        </p>
+        <Button
+          className="mt--8px mb--16px"
+          sx={{ fontSize: '20px' }}
+          type="button"
+          variant="contained"
+          onClick={() => navigate('/')}
+        >
+          {t('come_back')}
+        </Button>
+      </Card>
+    </Box>
   );
 };
 
